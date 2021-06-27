085ad8ed357ca708430861076843f19ef61865e8
Mango0x45 <thomasvoss@live.com>
2021-06-27 13:36:35 +0200

    [tests] Improve sed usage

--- tests/test.sh
+++ tests/test.sh
@@ -7,7 +7,8 @@ BIN=../smg
 cd "$(dirname "$0")"
 
 for t in $(ls * | grep -v '\.'); do
-	VOUT=$(valgrind -s $BIN $t 2>&1 | sed -Ef memory-leaks.sed)
+	VOUT=$(valgrind -s $BIN $t 2>&1 |
+		sed -En 's/==[0-9]+==\s+(.* lost: [1-9][0-9]* bytes in [1-9][0-9]* blocks)/\1/p')
 	[ -n "$VOUT" ] && printf '==> %s <==\n%s\n' $t "$VOUT"
 	$BIN $t | man -P less /dev/stdin
 done
